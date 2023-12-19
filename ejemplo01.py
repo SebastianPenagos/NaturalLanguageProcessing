6724dff567e4804dd0b79833b7cf786ec72cba0f
Sebastian Penagos Velasquez <raibol431@gmail.com>
2023-12-18 23:02:25 -0500

    First Version

---
+++ ejemplo01.py
@@ -0,0 +1,21 @@
+#Dividir el texto en oraciones
+
+import nltk
+
+# Cargar el texto
+nombre_archivo = "nlp.txt"
+archivo = open(nombre_archivo, "r", encoding="utf-8")
+text = archivo.read()
+
+print(text)
+
+# Tokenizar el texto en oraciones
+
+tokenizador = nltk.data.load('tokenizers/punkt/english.pickle')
+oraciones = tokenizador.tokenize(text)
+
+# Imprimir las oraciones
+
+for oracion in oraciones:
+    print(oracion)
+    print("")
